5df77460515e76ca3e705a81aff6bd28d755a71c
ArakiTakaki <arakitakaki.work@gmail.com>
2022-03-13 22:19:13 +0900

    feat: onProgressの定義

--- src/core.ts
+++ src/core.ts
@@ -1,11 +1,4 @@
 import { CannceledError } from './errors';
-/**
- * Promiseを直列実行させるための関数
- * @param values 同期処理をするPromise群
- * @returns result.value Promiseを返却
- * @returns result.cancel() 実行時にキャンセル
- * @returns result.progress() 現在の進捗 0-1
- */
 
 interface PromiseSerialResult<T extends readonly unknown[] | []> {
     value: Promise<T>;
@@ -14,9 +7,17 @@ interface PromiseSerialResult<T extends readonly unknown[] | []> {
 
 // TODO 随時追加
 interface PromiseSerialOptions<T> {
-    onProgress?: (value: number, result: T) => void
+    onProgress?: (value: number, index: number, result: T) => void
 }
 
+/**
+ * Promiseを直列実行させるための関数
+ * @param values 同期処理をするPromise群
+ * @param options.onProgress 進行状況を返却するコールバックの定義
+ * @returns result.value Promiseを返却
+ * @returns result.cancel() 実行時にキャンセル
+ * @returns result.progress() 現在の進捗 0-1
+ */
 export const promiseSerial = <T extends Promise<any>>(values: (() => T)[], {
     onProgress
 }: PromiseSerialOptions<T> = {}): PromiseSerialResult<T[]> => {
@@ -27,7 +28,7 @@ export const promiseSerial = <T extends Promise<any>>(values: (() => T)[], {
         for (let i = 0; i < values.length; i ++ ) {
             try {
                 const result = await values[i]();
-                if (onProgress != null) onProgress(i / values.length, result);
+                if (onProgress != null) onProgress((i + 1) / values.length, i, result);
                 if (isCancel) throw new CannceledError<T>(results);
                 results.push(result);
             } catch (err) {
