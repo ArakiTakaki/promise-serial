cbf2b460ed129ac90c3a544e0720a2f982525367
ArakiTakaki <arakitakaki.work@gmail.com>
2022-03-13 23:09:51 +0900

    feat: timeoutを設定

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,5 @@
 import { CannceledError } from './errors';
+import { debounce } from './utils';
 
 interface PromiseSerialResult<T extends readonly unknown[] | []> {
     value: Promise<T>;
@@ -8,23 +9,36 @@ interface PromiseSerialResult<T extends readonly unknown[] | []> {
 // TODO 随時追加
 interface PromiseSerialOptions<T> {
     onProgress?: (value: number, index: number, result: T) => void
+    timeout?: number;
 }
 
 /**
  * Promiseを直列実行させるための関数
  * @param values 同期処理をするPromise群
  * @param options.onProgress 進行状況を返却するコールバックの定義
+ * @param options.timeout 各イベントのタイムアウト期間 (初期値：50秒)
  * @returns result.value Promiseを返却
  * @returns result.cancel() 実行時にキャンセル
  * @returns result.progress() 現在の進捗 0-1
  */
 export const promiseSerial = <T extends Promise<any>>(values: (() => T)[], {
-    onProgress
+    onProgress,
+    timeout = 50000,
 }: PromiseSerialOptions<T> = {}): PromiseSerialResult<T[]> => {
+    let isComplete = false;
     let isCancel = false;
 
+    const results: T[] = [];
+    const timeoverEvent = debounce(() => {
+        if (isComplete) return;
+        const error = new CannceledError<T>(results);
+        error.message = 'promise is timeout';
+        if (isCancel) throw error;
+    }, timeout);
+
     const main = async () => {
-        const results: T[] = [];
+        timeoverEvent();
+
         for (let i = 0; i < values.length; i ++ ) {
             try {
                 const result = await values[i]();
@@ -35,6 +49,7 @@ export const promiseSerial = <T extends Promise<any>>(values: (() => T)[], {
                 throw err;
             }
         }
+        isComplete = true;
         return results;
     };
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,18 @@
 
 export const call = <T extends (...args: any[]) => any>(cb: T, ...data: Parameters<T>) => (): ReturnType<T> => cb(...data);
+
+/**
+ * イベントを呼び出し後、次のイベントまで指定した時間が経過するまではイベントを発生させない処理。
+ * @param callback
+ * @param wait (ms)
+ */
+export const debounce = <T extends (...args: any[]) => any>(func: T, wait: number = 500): (...args: Parameters<T>) => void => {
+  let cancelToken: number;
+  const callback = (...args: Parameters<T>) => {
+    window.clearTimeout(cancelToken);
+    cancelToken = window.setTimeout(() => {
+      func(...args);
+    }, wait);
+  }
+  return callback;
+};
