b0437b2fe73522ad087e3014906da5a07e24c0eb
ArakiTakaki <arakitakaki.work@gmail.com>
2022-03-14 08:23:07 +0900

    docs: update document

--- src/core.ts
+++ src/core.ts
@@ -17,7 +17,7 @@ interface PromiseSerialOptions<T> {
  * Promiseを直列実行させるための関数
  * @param values 同期処理をするPromise群
  * @param options.onProgress 進行状況を返却するコールバックの定義
- * @param options.timeout 各イベントのタイムアウト期間 (初期値：50秒)
+ * @param options.timeout 各イベントのタイムアウト期間 (初期値：無限)
  * @param options.isNotCancelledThrow canncel時にthrowするかどうか
  * @returns result.value Promiseを返却
  * @returns result.cancel() 実行時にキャンセル
